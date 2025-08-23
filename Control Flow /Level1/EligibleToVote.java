d776c56f2f552ab890d3beaa0e3e32d4b209b67f
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-08-23 14:28:41 +0530

    EligibleToVote.java

--- Control Flow /Level1/EligibleToVote.java
+++ Control Flow /Level1/EligibleToVote.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 		    public static void main(String[] args) {
 		        Scanner scanner = new Scanner(System.in);
 
-		        // Input
 		        System.out.print("Enter the person's age: ");
 		        int age = scanner.nextInt();
-
-		        // Check voting eligibility
 		        if (age >= 18) {
 		            System.out.println("The person's age is " + age + " and can vote.");
 		        } else {
@@ -20,3 +17,4 @@ import java.util.Scanner;
 		    }
 
 }
+
