ab47edd942046df22746d2493dddf8ef86947241
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-08-31 19:43:44 +0530

    Add files via upload

---
+++ Methods/Level1/Season.java
@@ -0,0 +1,27 @@
+package methods;
+import java.util.Scanner;
+public class Season {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int month = scanner.nextInt();
+	        int day = scanner.nextInt();
+	        scanner.close();
+
+	        if (isSpringSeason(month, day)) {
+	            System.out.println("Its a Spring Season");
+	        } else {
+	            System.out.println("Not a Spring Season");
+	        }
+	    }
+
+	    public static boolean isSpringSeason(int month, int day) {
+	        if ((month == 3 && day >= 20) ||
+	            (month == 4) ||
+	            (month == 5) ||
+	            (month == 6 && day <= 20)) {
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+	}
