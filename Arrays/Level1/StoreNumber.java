d53f277216a551fd4a1546f89e324b833da7612e
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-08-31 20:54:32 +0530

    Add files via upload

---
+++ Arrays/Level1/StoreNumber.java
@@ -0,0 +1,24 @@
+package Arrays;
+	import java.util.Scanner;
+
+	public class StoreNumber{
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        double[] numbers = new double[10];
+	        double total = 0.0;
+	        int index = 0;
+	        while (true) {
+	            double input = sc.nextDouble();
+	            if (input <= 0 || index == 10) {
+	                break;
+	            }
+	            numbers[index] = input;
+	            index++;
+	        }
+	        for (int i = 0; i < index; i++) {
+	            total += numbers[i];
+	            System.out.println(numbers[i]);
+	        }
+	        System.out.println("Total = " + total);
+	    }
+}
